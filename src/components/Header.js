3618fda2e7949e6a5eeaf1a1157c4a86b2e37623
junaid <jaanusd79@gmail.com>
2024-01-08 13:31:34 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,14 @@ import SearchIcon from '@mui/icons-material/Search';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import PhotoCameraFrontIcon from '@mui/icons-material/PhotoCameraFront';
+import { YT_LOGO } from '../utils/constants';
 
 const Header = () => {
     return (
         <div className="px-4 py-2 flex justify-between items-center">
             <div className="flex items-center">
                 <span className="mx-2 hover:bg-gray-300 p-2 hover:bg-opacity-40 hover:rounded-full cursor-pointer"><MenuIcon /></span>
-                <img className="w-28 cursor-pointer" src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" alt="YT-LOGO" />
+                <img className="w-28 cursor-pointer" src={YT_LOGO} alt="YT-LOGO" />
             </div>
             <div className="w-1/3 flex">
                 <input type="text" className="w-full rounded-full px-4 py-1 border border-black rounded-tr-none rounded-br-none" />
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const YT_LOGO = "https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg";
