d8444fb051829b08325593ae7509a134662b5647
junaid <jaanusd79@gmail.com>
2024-02-15 19:13:44 +0530

    Implemented Video Shimmer

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,12 +1,13 @@
 import Card from "./Card";
 import usePopularVideos from "../hooks/usePopularVideos";
 import { Link } from "react-router-dom";
+import VideoCardShimmer from "./ShimmerUi/videoCardShimmer";
 
 const CardContainer = () => {
 
     const popularVideos = usePopularVideos();
 
-    if (!popularVideos) return;
+    if (!popularVideos) return <VideoCardShimmer />;
 
     return (
         <div className="mt-4 flex flex-wrap justify-center lg:justify-start gap-5">
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import ButtonsContainer from './ButtonsContainer';
 import CardContainer from './CardContainer';
-import VideoCardShimmer from './ShimmerUi/videoCardShimmer';
 
 const SecondaryContainer = () => {
     return (
         <div className=" md:pl-6">
             <ButtonsContainer />
-            <VideoCardShimmer />
-            {/* <CardContainer /> */}
+            <CardContainer />
         </div>
     )
 }
