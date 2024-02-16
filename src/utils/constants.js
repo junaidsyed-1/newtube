5238805956854d4557af72d6527581fead45e3bb
junaid <jaanusd79@gmail.com>
2024-02-16 21:37:43 +0530

    Updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ const YT_API = process.env.REACT_APP_YT_API_KEY;
 
 export const YT_LOGO = "https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg";
 
-export const YT_POPULAR_VIDEO = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=25&chart=mostPopular&regionCode=IN&key=" + YT_API;
+export const YT_POPULAR_VIDEO = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50&chart=mostPopular&regionCode=IN&key=" + YT_API;
 
 export const EXPLORE_SP = ["Trending", "Music", "Films", "Live", "Gaming", "News", "Podcast", "Sports", "Learning"];
 
